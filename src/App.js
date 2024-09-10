ae657869ce863c6331aa856ef04b53514f9b5f08
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-09-10 17:54:31 +0200

    X wordreverse

--- src/App.js
+++ src/App.js
@@ -3,108 +3,179 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [text, setText] = useState('');
-  const [messages, setMessages] = useState([]);
-  const [isError, setIsError] = useState(false);
-  const [responseMessage, setResponseMessage] = useState('');
-  const [title, setTitle] = useState('Insert the word to reverse');
+  const [file, setFile] = useState(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [isUploading, setIsUploading] = useState(false);
 
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css';
+    document.head.appendChild(link);
+  }, []);
 
-  const handleChange = (event) => {
-    setText(event.target.value);
+  const handleFileSelect = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    setUploadProgress(0);
+    setSuccess(false);
   };
 
-  const handleClick = async () => {
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setIsDragging(false);
+    const droppedFile = event.dataTransfer.files[0];
+    setFile(droppedFile);
+    setUploadProgress(0);
+    setSuccess(false);
+  };
+
+  const uploadFile = async () => {
+    if (!file) {
+        alert('Nessun file selezionato.');
+        return;
+    }
+
+    if (isUploading) {
+      alert('Attendi il completamento dell\'upload attuale.');
+      return;
+    }
+
+    setIsUploading(true);
+
     try {
-      if (text) {
-        console.log('Sending message:', text);
-  
-        const response = await axios.post('https://a9icm55wze.execute-api.us-east-1.amazonaws.com/prod/process', {
-          message: text,
+        // Step 1: Ottieni la URL presigned
+        const response = await axios.post('https://hym80goqc7.execute-api.us-east-1.amazonaws.com/prod/get-url-presigned', { //DA CAMBIARE
+            filename: file.name
         }, {
-          headers: {
-            'Content-Type': 'application/json'
-          }
+            headers: {
+                'Content-Type': 'application/json'
+            }
         });
-  
-        console.log('API response:', response);
-  
-        // Verifica che la risposta sia quella attesa
-        if (response.status === 200 && response.data && response.data.message) {
-          setMessages(prevMessages => [...prevMessages, response.data.message]);
-          setResponseMessage(`Message sent successfully!`);
-          setIsError(false);
-        } else {
-          // Mostra un messaggio generico se non si riceve il campo "message"
-          setResponseMessage('Message sent successfully, but no reversed message returned.');
-          setIsError(false);
+
+        const uploadUrl = response.data.url;
+        const bucketName = response.data.bucketName; 
+        const objectKey = response.data.key; 
+        console.log('URL presigned ricevuta:', uploadUrl);
+        //console.log('Nome del bucket:', bucketName);
+        //console.log('Object key:', objectKey);
+        setError('');
+
+        // Step 2: Carica il file usando la URL presigned
+        await axios.put(uploadUrl, file, {
+          headers: {
+          'Content-Type': 'text/plain'
+        },
+        onUploadProgress: (progressEvent) => {
+          const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+          setUploadProgress(percentCompleted);
         }
-        setText('');
-        setTitle('Insert another word');
-        setTimeout(() => {
-          setResponseMessage('');
-        }, 20000);
-  
-      } else {
-        setResponseMessage('The message field is empty.');
-        setIsError(true);
-        setTimeout(() => {
-          setResponseMessage('');
-        }, 20000);
-      }
+      });
+
+      // Step 3: Invia i dettagli del file all'API
+      await axios.post('https://vzxoqe9982.execute-api.us-east-1.amazonaws.com/dev/setretrieveinfo', { //DA CAMBIARE
+        bucketname: bucketName,
+        objectkey: objectKey,
+      }, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      });
+
+      // Aggiorna l'elenco dei file caricati
+      setUploadedFiles(prevFiles => [
+        ...prevFiles,
+        { name: file.name, status: 'Caricamento completo', color: 'red' } 
+      ]);
+      setSuccess(true);
+      setFile(null); // Resetta il file
+      setUploadProgress(0);
+      alert('File caricato con successo!');
+
     } catch (error) {
-      console.error('Error response:', error.response); 
-      if (!error.response || error.response.status === 504) {
-        // Considera un timeout o un errore CORS come un timeout
-        setResponseMessage('Request timed out.');
-      } else {
-        setResponseMessage(`Error sending message: ${error.response.status}`);
-      }
-      setIsError(true);
-      setTimeout(() => {
-        setResponseMessage('');
-      }, 20000);
+      console.error('Errore durante il caricamento del file:', error);
+      setError('Errore durante il caricamento del file');
+    } finally {
+      setIsUploading(false); // Reset dello stato di caricamento
     }
   };
 
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      handleClick();
+  useEffect(() => {
+    if (success) {
+      const timer = setTimeout(() => {
+        setSuccess(false); // Nascondi il messaggio di successo dopo 10 secondi
+      }, 10000);
+      return () => clearTimeout(timer);
     }
-  };
+  }, [success]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="title">{title}</h1>
+    <div className="uploader-container">
+      <div
+        className={`dropzone ${isDragging ? 'dragging' : ''}`}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
+        {file ? (
+          <p>{file.name}</p>
+        ) : (
+          <p>Trascina un file qui o clicca per selezionare un file</p>
+        )}
         <input
-          type="text"
-          value={text}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-          placeholder="Enter your message"
+          type="file"
+          onChange={handleFileSelect}
+          style={{ display: 'none' }}
+          id="fileInput"
         />
-        <button onClick={handleClick}>
-          Send Message
-        </button>
-        {responseMessage && (
-          <p className={isError ? 'error-message' : 'success-message'}>
-            {responseMessage}
-          </p>
-        )}
-        {messages.length > 0 && (
-          <div className="messages-container">
-            <h2>Reversed words:</h2>
-            <ul>
-              {messages.map((msg, index) => (
-                <li key={index}>{msg}</li>
-              ))}
-            </ul>
+      </div>
+      <button className="btn" onClick={() => document.getElementById('fileInput').click()}>Seleziona File</button>
+      <button className="btn" onClick={uploadFile} style={{ marginLeft: '10px' }}>
+        Carica File
+      </button>
+
+      
+      {uploadProgress > 0 && (
+        <div className="progress-container">
+          <div className="progress-bar" style={{ width: `${uploadProgress}%` }}>
+            {uploadProgress}%
           </div>
-        )}
-      </header>
+        </div>
+      )}
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {success && <p style={{ color: 'green' }}>Caricamento completato con successo resta in attesa!</p>}
+
+      {uploadedFiles.length > 0 && (
+        <>
+          <h2 className="files-title">Matrici in elaborazione o elaborate</h2>
+          <ul className="uploaded-files-list">
+            {uploadedFiles.map((uploadedFile, index) => (
+              <li key={index}>
+                <span>{uploadedFile.name} - {uploadedFile.status}</span>
+                <i className={`fas fa-circle ${uploadedFile.color === 'red' ? 'semaforo red' : 'semaforo green'}`}></i>
+                </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
 
 export default App;
+
+
